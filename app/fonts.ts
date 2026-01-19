8876ec942e86339ac9741e3fa94f8cfb3c4392b6
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2026-01-19 16:22:16 +0600

    applied comfortaa font to the main content of admin panel

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+// app/fonts.ts
+import { Comfortaa } from "next/font/google";
+
+export const comfortaa = Comfortaa({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-comfortaa",
+});
