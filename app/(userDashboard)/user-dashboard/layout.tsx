c9822fbd42f419544e26de1cad3bf0c800e29132
ahsansoftvence-omega <ahsansoftvence@gmail.com>
2026-01-21 16:33:32 +0600

    Created chat bot fixed icon

--- app/(userDashboard)/user-dashboard/layout.tsx
+++ app/(userDashboard)/user-dashboard/layout.tsx
@@ -1,5 +1,9 @@
-// app/(userDashboard)/user-dashboard/layout.tsx
-import UserHomeNavbar from "./userNavbar/page";
+"use client";
+
+import DashboardLayout from "@/components/layout/dashboard/DashboardLayout";
+import UserSidebar from "@/components/layout/dashboard/UserSidebar";
+import RoleGuard from "@/components/auth/RoleGuard";
+import ChatBot from "@/features/ChatBot";
 
 type UserDashboardLayoutProps = {
   children: React.ReactNode;
@@ -9,17 +13,11 @@ export default function UserDashboardLayout({
   children,
 }: UserDashboardLayoutProps) {
   return (
-    <div className="min-h-screen bg-gray-50 relative">
-      {/* Fixed Navbar */}
-      <UserHomeNavbar />
-
-      {/* Main content with padding to avoid overlap with fixed navbar */}
-      <main className="mt-26 px-6 flex flex-col items-center">
-        {/* pt-[104px] = navbar height (adjust if your navbar height changes) */}
-        <div className="w-full container mx-auto ">
-          {children}
-        </div>
-      </main>
-    </div>
+    <RoleGuard allowedRoles={["USER", "ADMIN", "MODERATOR"]}>
+      <DashboardLayout sidebar={<UserSidebar />}>
+        <ChatBot />
+        {children}
+      </DashboardLayout>
+    </RoleGuard>
   );
 }
