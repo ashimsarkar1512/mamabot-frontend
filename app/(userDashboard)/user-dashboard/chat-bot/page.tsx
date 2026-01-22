497367723c8dc8410f416dc720e73e69e86f7048
ahsansoftvence-omega <ahsansoftvence@gmail.com>
2026-01-22 12:09:47 +0600

    Updated chatbot UI

--- app/(userDashboard)/user-dashboard/chat-bot/page.tsx
+++ app/(userDashboard)/user-dashboard/chat-bot/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import Link from "next/link";
+
 export default function ChatBotPage() {
-    return (
-        <div>
-            <h1>ChatBot</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>
+        ChatBot Page -{" "}
+        <Link
+          href="/chatBot"
+          className="text-pink-500 font-medium hover:text-pink-600 hover:cursor-pointer transition-transform duration-300"
+        >
+          Go to ChatBot
+        </Link>
+      </h1>
+    </div>
+  );
 }
