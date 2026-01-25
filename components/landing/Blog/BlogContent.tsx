c61db0ae80dce93409a1e8db346c72e0762b6bad
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2026-01-25 10:12:47 +0600

    filter in blog posts done

--- components/landing/Blog/BlogContent.tsx
+++ components/landing/Blog/BlogContent.tsx
@@ -1,44 +1,55 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import BlogCard from "./BlogCard";
 import { blogPosts } from "@/lib/data/blogData";
-
+const categories = [
+  "View All",
+  "Pregnancy",
+  "Nutrition",
+  "Baby Care",
+  "Mental Health",
+  "Exercise",
+  "Postpartum",
+];
 const BlogContent: React.FC = () => {
+  const [activeCategory, setActiveCategory] = useState("View All");
+
+  const filteredBlogs =
+    activeCategory === "View All"
+      ? blogPosts
+      : blogPosts.filter((post) => post.category === activeCategory);
   return (
     <section className="">
       <div className="">
         {/* Tabs / Categories */}
         <div className="flex flex-wrap justify-around bg-white/25 border-2 !border-white mb-10">
-          {[
-            "View All",
-            "Pregnancy",
-            "Nutrition",
-            "Baby Care",
-            "Mental Health",
-            "Exercise",
-            "Postpartum",
-          ].map((cat) => (
+          {categories.map((cat) => (
             <button
               key={cat}
-              className={`px-5 py-2.5 text-start cursor-pointer hover:bg-primary hover:text-white text-sm font-medium transition-all ${
-                cat === "View All"
-                  ? "bg-primary text-white "
-                  : " text-gray-700 "
-              }`}
+              onClick={() => setActiveCategory(cat)}
+              className={`px-5 py-2.5 text-sm font-medium transition-all cursor-pointer
+                ${
+                  activeCategory === cat
+                    ? "bg-primary text-white"
+                    : "text-gray-700 hover:bg-primary hover:text-white"
+                }
+              `}
             >
               {cat}
             </button>
           ))}
         </div>
 
-       
-
         {/* Blog Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ">
-          {blogPosts.map((post) => (
-            <BlogCard key={post.id} post={post} />
-          ))}
+          {filteredBlogs.length > 0 ? (
+            filteredBlogs.map((post) => <BlogCard key={post.id} post={post} />)
+          ) : (
+            <p className="col-span-full text-center text-gray-500">
+              No articles found in this category.
+            </p>
+          )}
         </div>
 
         {/* Load More */}
